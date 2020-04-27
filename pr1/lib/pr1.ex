47c7431a759a8d9c899216e78c27ee372f96d162
tectiv3 <git@getpanda.me>
2020-04-28 00:45:28 +0900

    Initial commit

---
+++ pr1/lib/pr1.ex
@@ -0,0 +1,18 @@
+defmodule Pr1 do
+  @moduledoc """
+  Documentation for `Pr1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pr1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ pr1/test/pr1_test.exs
@@ -0,0 +1,8 @@
+defmodule Pr1Test do
+  use ExUnit.Case
+  doctest Pr1
+
+  test "greets the world" do
+    assert Pr1.hello() == :world
+  end
+end
